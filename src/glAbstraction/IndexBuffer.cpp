d9394b4b263fc5ff539f94c698c3e19e51a6edd3
LucasV <lucas.violent69@gmail.com>
2024-10-23 15:23:13 +0200

    Refactor: Created a new folder for all opengl abstraction code

---
+++ src/glAbstraction/IndexBuffer.cpp
@@ -0,0 +1,23 @@
+#include "IndexBuffer.h"
+
+namespace lgl {
+    IndexBuffer::IndexBuffer(const unsigned int *data, const unsigned int count) : id(0), count(count) {
+        glGenBuffers(1, &id);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, id);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(unsigned int), data, GL_STATIC_DRAW);
+    }
+
+    IndexBuffer::~IndexBuffer() {
+        glDeleteBuffers(1, &id);
+    }
+
+    void IndexBuffer::Bind() const {
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, id);
+    }
+
+    void IndexBuffer::Unbind() const {
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    }
+
+    unsigned int IndexBuffer::GetCount() const { return count; }
+}
---
+++ src/glAbstraction/IndexBuffer.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <glad/glad.h>
+
+#include "GL/gl.h"
+
+namespace lgl {
+    class IndexBuffer {
+    public:
+        IndexBuffer(const unsigned int *data, unsigned int count);
+
+        ~IndexBuffer();
+
+        void Bind() const;
+
+        void Unbind() const;
+
+        [[nodiscard]] unsigned int GetCount() const;
+
+    private:
+        GLuint id;
+        unsigned int count;
+    };
+}
